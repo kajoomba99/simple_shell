6dae89e2c898a0a3161f2dcdb5b0fab3491da4d4
melandres8 <melanmosta@gmail.com>
2020-04-04 23:59:44 +0000

    Update, adding eof_func

--- shell.h
+++ shell.h
@@ -8,8 +8,11 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <signal.h>
+#include <stdlib.h>
 
 /*Signal fucntion*/
 void handle_signal(int sig);
+/*EOF function*/
+void eof_func(char c);
 
 #endif
