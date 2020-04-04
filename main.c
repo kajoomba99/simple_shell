9498e03e810735f006427c613410db155f322700
Steven Brand <steven.brand1999@gmail.com>
2020-04-04 12:30:55 -0500

    Siple shell functionality, receibes the complete path of the executable file and header with the libraries needed so far

---
+++ main.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+
+/**
+ * main - entry point
+ * Return: 0 on succes
+ */
+
+int main(void)
+{
+	struct stat statbuf;
+	ssize_t rgetl;
+	char *lineptr;
+	size_t n = 0;
+	pid_t rfork;
+	char *ctok;
+	int wstatus, rstat;
+	char *argv[2];
+
+	argv[1] = NULL;
+
+	do {
+		printf("#cisfun$ ");
+		rgetl = getline(&lineptr, &n, stdin);
+		ctok = strtok(lineptr, "\n");
+		argv[0] = lineptr;
+		rfork = fork();
+		if (rfork == 0)
+		{
+			rstat = stat(argv[0], &statbuf);
+
+			if (rstat == 0)
+				execve(argv[0], argv, NULL);
+			else
+				perror("Error");
+			return (0);
+				}
+		else
+		{
+			wait(&wstatus);
+		}
+
+	} while (rgetl != -1);
+}
---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+#endif
